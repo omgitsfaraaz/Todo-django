bc1183aff6f4692b7293162167b21c1be71ce057
unknown <mohsinsait23@gmail.com>
2020-11-01 15:09:56 +0530

    added post method to update task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -19,6 +19,12 @@ def index(request):
 def updateTask(request, pk):
 	task = Task.objects.get(id=pk)
 	form = TaskForm(instance=task)
+
+	if request.method == 'POST':
+		form = TaskForm(request.POST, instance=task)
+		if form.is_valid():
+			form.save()
+			return redirect('/')
 	#https://youtu.be/4RWFvXDUmjo
 	context = {'form': form}
 	return render(request, 'tasks/update_task.html', context)
