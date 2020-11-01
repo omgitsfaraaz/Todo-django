496ea474b3b3383fd1f4fc9b46ea3d35aed943bc
unknown <mohsinsait23@gmail.com>
2020-11-01 15:46:05 +0530

    able to delete each task successfully

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -31,6 +31,9 @@ def updateTask(request, pk):
 
 def deleteTask(request, pk):
 	item = Task.objects.get(id=pk)
+	if request.method == 'POST':
+		item.delete()
+		return redirect('/')
 	context = {'item': item}
 
 	return render(request, 'tasks/delete.html', context)
